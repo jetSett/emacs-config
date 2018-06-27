6363bcd5714e95abb578cbe466f8737c4320bdc4
jetSett <joel.felderhoff@ens-lyon.fr>
2018-06-27 23:16:42 +0200

    Ui modif

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,14 +1,27 @@
 (require 'init-elpa)
 
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/dracula-theme-20170412.845")
-;(load-theme 'dracula t)
-
 (require-package 'atom-one-dark-theme)
 (require-package 'golden-ratio)
 
+(require 'golden-ratio)
+
 (setq inhibit-startup-message t)
+
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+
 (set-face-attribute 'default nil :height 140)
 (setq-default line-spacing 0.4)
+
+(setq
+      x-select-enable-clipboard t
+      x-select-enable-primary t
+      save-interprogram-paste-before-kill t
+      apropos-do-all t
+      mouse-yank-at-point t)
 
 (load-theme 'atom-one-dark t)
 
